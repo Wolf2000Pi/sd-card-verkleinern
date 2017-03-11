938d08439fa12b35df9b4410a28fb3c2a03f6ad0
Wolf2000 <wolf2000@aon.at>
2017-03-11 13:37:23 +0100

    na

--- shrink.sh
+++ shrink.sh
@@ -3,10 +3,8 @@
 IMG="$1"
 
 if [[ -e $IMG ]]; then
-  P_START=$( fdisk -lu $IMG | grep Linux | awk '{print $2}' ) # Start of 
-2nd partition in 512 byte sectors
-  P_SIZE=$(( $( fdisk -lu $IMG | grep Linux | awk '{print $3}' ) * 1024 
-)) # Partition size in bytes
+  P_START=$( fdisk -lu $IMG | grep Linux | awk '{print $2}' ) # Start of 1nd partition in 512 byte sectors
+  P_SIZE=$(( $( fdisk -lu $IMG | grep Linux | awk '{print $3}' ) * 1024 )) # Partition size in bytes
   losetup /dev/loop1 $IMG -o $(($P_START * 512)) --sizelimit $P_SIZE
   fsck -f /dev/loop1
   resize2fs -M /dev/loop1 # Make the filesystem as small as possible
